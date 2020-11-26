6256aa227cd4cd9dbacb75b6cc590c1fb543860a
Nico Bastias <nbastias@holos.cl>
2020-11-26 13:41:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a test
         </a>
       </header>
     </div>
